4b0d3d8374724b56b719a2a39791fec5ad8e6304
hhnnhh <32333241+hhnnhh@users.noreply.github.com>
2021-01-23 18:21:01 +0100

    updated version with unique names table in second tab

--- server.R
+++ server.R
@@ -1,58 +1,57 @@
+
+#Hannah Bohle, 11.01. 2021
 library(shiny)
+#library(shinythemes)
+library(dplyr)
 library(wordcloud)
-library(RColorBrewer)
-library(profvis)
+#library(DT)
+#library(here)
 
-options(shiny.maxRequestSize=10*1024^2) 
-#profvis({
-server <- function(input, output,session) {
-  #df <- read.csv("D:/Dropbox/R_wissen/berlin_names_year/berlinnames_shiny/Berlin_2019_with_new_columnname.csv")
-  #df_check <- read.csv("data/berlin_with_year_final.csv")
-  #df <- read.csv("data/Berlin_with_year.csv")
-  df <- read.csv("data/Berlin_with_year.csv")
+#df <- read.csv("data/Berlin_with_year.csv")
+#df <- read.csv("D:/Dropbox/R_wissen/berlin_name_year/data/Berlin_with_year.csv")
+
+
+server <- function(input, output) {
+  df <- read.csv("data/Berlin_with_year_final.csv")
+  filtered_kiez <- reactive({
+    df %>%
+        filter(Kiez == input$kiezId)
+  })
   
+  filtered_gender <- reactive({
+      filtered_kiez() %>% 
+        filter(geschlecht == input$genderId)
+  })
   
-  #observeEvent(input$yearId, {
-  #observeEvent(D1(),{
-   # updateSelectInput(session, "yearId", "Year",  choices = unique(D1()$year),selected = unique(D1()$year)[1])
+
+  filtered_year <- reactive({
+    filtered_gender() %>% 
+      filter(year == input$yearId) 
+  })
+  
+   one_filtered <- reactive({
+     filtered_year() %>% 
+       filter(filtered_year()$anzahl == 1)
+   })
+  
+  fully_filtered <- eventReactive(input$select, {
+    filtered_year()
+  })
+
+  output$plot <- renderPlot({
+    p<-wordcloud(words = fully_filtered()$vorname, freq = fully_filtered()$anzahl, colors=brewer.pal(8,"BrBG"),min.freq = 10, max.words=200, random.order=FALSE, rot.per=0.35,scale=c(3.5,0.25))
+    print(p)
+  })
+  
+  output$result <-renderTable({
+    one_filtered()
+  })
+  
+  # output$plot2 <- renderPlot({
+  #   p<-wordcloud(words = fully_filtered()$vorname, freq = fully_filtered()$anzahl, colors=brewer.pal(8,"BrBG"),min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35,scale=c(3.5,0.25))
+  #   print(p)
   #})
-  
-  observeEvent(D1(),{
-    updateSelectInput(session, "selectinputid", "Kiez to Select:",  choices = unique(D1()$Kiez),selected = unique(D1()$Kiez)[1])
-  })
-  
-  D0  <- reactive({
-    df[df$year %in% input$yearId,]
-  })
-  
-  D1  <- reactive({
-    D0()[D0()$geschlecht %in% input$genderId,]
-  })
-  
-  D2 <- eventReactive(input$goButton1,{
-    D1()[D1()$Kiez %in% input$selectinputid,]
-   #D1()[D1()$year %in% input$yearId,]
-  })
-
-
-   output$plot <- renderPlot({
-     # if (input$genderId == 'w'){
-     #   color=brewer.pal(8,"BrBG")
-     # } 
-     # else{
-     #   color=brewer.pal(8, "PRGn")
-     # }
-
-     #ggplot(D2(),aes(x=vorname,y=anzahl, label=vorname)) + geom_text_interactive(stat = "identity")
-     #treemap(D2(),index = c("vorname"),vSize ="anzahl")
-     p<-wordcloud(words = D2()$vorname, freq = D2()$anzahl, colors=brewer.pal(8, "PRGn"),min.freq = 10, max.words=200, random.order=FALSE, rot.per=0.35,scale=c(3.5,0.25))
-  print(p)
- })
-
-  
-  #ploting results as table
-  # output$result <- renderTable({
-  #   D2()
-  # })
 }
-#})
+
+# Run the application 
+#shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -1,44 +1,72 @@
+
+#Hannah Bohle, 11.01. 2021
 library(shiny)
-library(ggplot2)
+#library(shinythemes)
+library(dplyr)
+library(wordcloud)
+#library(DT)
+#library(here)
+
+#load(here::here('apps', 'df', 'df.RData'))
+df <- read.csv("data/Berlin_with_year_final.csv")
+#df <- read.csv("D:/Dropbox/R_wissen/berlin_name_year/data/Berlin_with_year.csv")
 
 
-#https://stackoverflow.com/questions/46348193/shiny-automatic-selectinput-value-update-based-on-previous-filter
-#df <- read.csv("D:/Dropbox/R_wissen/berlin_names_year/berlinnames_shiny/Berlin_2019_with_new_columnname.csv")
-df <- read.csv("data/Berlin_with_year.csv")
+
+min_year <- min(df$year)
+max_year <- max(df$year)
 
 
-shinyUI(
-  fluidPage(
-    titlePanel("How are children in Berlin named?"),
-    sidebarLayout(
+ui <- fluidPage(
+  titlePanel("How are children in Berlin named?"),
+  sidebarLayout(
       sidebarPanel(
-        helpText("Displaying given first names in Berlin by year, gender, and Kiez. source: Berlin open data."),
+        helpText("Displaying frequency of first names in Berlin by Kiez, gender, and year. source: Berlin open data."),
         
-        selectInput("yearId", "Year", choices = unique(df$year)),
+        #width = 2,
+                 selectInput(
+                   inputId = "kiezId",
+                   label = "Kiez",
+                   choices = unique(df$Kiez),
+                   selected = "Charlottenburg-Wilmersdorf"
+                 ),
         
-        helpText("Sorry, data was only available for binary gender :("),
-        selectInput("genderId", "Gender", choices = unique(df$geschlecht)),
-        helpText("Based on the gender, now select Kiez below to display the first names in the main panel."),
-        
-        selectInput("selectinputid", "Kiez to Select:", choices = unique(df$Kiez)),
-        #checkboxInput("positionid", "Only first names?", value = FALSE, width = NULL),
-        actionButton("goButton1", "Submit Choice")),
-      
-      mainPanel(
-        #tableOutput("result")
-        plotOutput('plot')
-        )
-        
+        helpText("Data only available for binary gender.."),
+                 selectInput(
+                   inputId = "genderId",
+                   label = "Gender",
+                   choices = unique(df$geschlecht),
+                   selected = "w"
+                 ),
+                 
+
+                 sliderInput(
+                   inputId = "yearId",
+                   label = "Year", min = min_year, max = max_year,
+                   round = 2, 
+                   step = 1,
+                   sep = "",
+                   value = 2018
+                   
+                   
+                 ),
+                 
+                 br(), 
+                 
+                 actionButton('select', 'Select'),
+                 
+    ),
+    
+    mainPanel(
+      tabsetPanel(type = "tabs",
+      #tableOutput("result")
+      tabPanel("Frequent names", plotOutput("plot")),
+      tabPanel("Unique names", tableOutput("result"))
+      #  plotOutput('plot'),
+       # plotOutput('plot2')
       )
     )
   )
-#)
-  
+)
 
-#   mainPanel(
-#   #  tabsetPanel(
-#    #   tabPanel('girl', dataTableOutput(dataset$geschlecht == "w")),
-#     #  tabPanel('boy', dataTableOutput(dataset$geschlecht == "m")),
-#     plotOutput('plot')
-#   )
-# )
+
